378cea06272e48d41faf50378b84adb356ad1bc2
Jakub Pešek <pesek.jakub548@gmail.com>
2022-12-14 18:47:44 +0100

    + type declarations

---
+++ declarations/types.ts
@@ -0,0 +1,11 @@
+export type Absences = {
+	attended: number,
+	missed: number,
+	notExcused: number,
+}
+
+export type ChartColors = {
+	attended: string,
+	missed: string,
+	notExcused: string,
+}
